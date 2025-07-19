ef5bc9d01acc6269effce591fb93faf826aad8a5
cashlau <46700514+cashlau@users.noreply.github.com>
2025-07-19 02:11:28 -0700

    Update customize.sh

--- config/customize.sh
+++ config/customize.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
+
+# 如果源码根目录就是 openwrt ，就保留
 cd openwrt
 
-# 默认 IP
+# 修改默认 LAN IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
-# 时区与主机名
+# 设置时区和主机名
 sed -i "s/'UTC'/'Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='HUAWEI'/g" package/base-files/files/bin/config_generate
 
-# 默认主题（可选）
+# 修改默认主题为 argon，确保主题已存在
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
